2a57515e5748b54d35dc7a4b7ee74fcc2c517875
Artur <busanix@gmail.com>
2023-06-19 17:22:08 -0300

    dev

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+extern crate proc_macro;
+extern crate quote;
+extern crate syn;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Ident, ItemStruct};
+
+struct MeasuresInput {
+    value_type: Ident,
+}
+
+impl syn::parse::Parse for MeasuresInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let value_type = input.parse::<Ident>()?;
+        Ok(MeasuresInput { value_type })
+    }
+}
+
+#[proc_macro_attribute]
+pub fn measures(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemStruct);
+
+    let args = parse_macro_input!(attr as MeasuresInput);
+
+    let value_type = args.value_type;
+
+    let struct_name = input.clone().ident;
+
+    // println!("item: \"{}\"", struct_name.to_string());
+
+    TokenStream::from(quote! {
+        #input
+
+        impl #struct_name {
+            const VALUE_TYPE: &'static str = stringify!(#value_type);
+        }
+    })
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
 }
